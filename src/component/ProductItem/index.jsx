487c636ce76741f92abb49f4334b0896cb2038ed
Yoga Worksheet <yoga.worksheet@gmail.com>
2022-11-01 07:51:23 +0700

    make alert when not signing in

--- src/component/ProductItem/index.jsx
+++ src/component/ProductItem/index.jsx
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addToCart } from "../../features/Cart/action";
 import { idrFormatter } from "../../utils/formatter";
+import Modal from "../../component/Modal";
 
 const ProductItem = ({ product }) => {
 	const dispatch = useDispatch();
-	const cart = useSelector((state) => state.cart);
+	const [modal, setModal] = useState("");
 	const addToCartHandler = () => {
 		const { _id, name, price, image_url } = product;
 		const payload = {
@@ -15,10 +16,21 @@ const ProductItem = ({ product }) => {
 			image_url,
 			qty: 1,
 		};
-		dispatch(addToCart(payload));
+		if (!localStorage.getItem("auth")) {
+			setModal("You must log in before using cart!");
+		} else {
+			dispatch(addToCart(payload));
+		}
 	};
 	return (
 		<div className="flex flex-col justify-between bg-[#ffffff] text-slate-600 rounded-3xl p-4 shadow-lg">
+			<Modal
+				icon={<ion-icon name="warning"></ion-icon>}
+				message={modal}
+				type="warning"
+				boolean={modal}
+				onClick={() => setModal("")}
+			/>
 			<div className="overflow-hidden rounded-2xl mb-3">
 				<img
 					src={`${process.env.REACT_APP_API_HOST}/images/products/${product.image_url}`}
@@ -27,12 +39,9 @@ const ProductItem = ({ product }) => {
 				/>
 			</div>
 			<div>
-				<a
-					href="#"
-					className="text-base font-semibold transition ease-in-out hover:text-blue-500"
-				>
+				<button className="text-base text-left font-semibold transition ease-in-out hover:text-blue-500">
 					{product.name}
-				</a>
+				</button>
 				<p className="text-xs font-light mb-3">{product.description}</p>
 			</div>
 			<h2 className="font-bold text-lg mb-3">
