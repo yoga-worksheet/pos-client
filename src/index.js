d168aa6ccd5bb5eb162230b11bcea0733b7a8840
yoga-worksheet <yoga.worksheet@gmail.com>
2023-06-06 16:13:22 +0700

    feat : add open replay tracker

--- src/index.js
+++ src/index.js
@@ -6,15 +6,27 @@ import router from "./route";
 import store from "./features/store";
 import { Provider } from "react-redux";
 import reportWebVitals from "./reportWebVitals";
+import OpenReplay from "@openreplay/tracker";
+import trackerAssist from "@openreplay/tracker-assist";
+const tracker = new OpenReplay({
+  projectKey: "brccs3yWe4TloO9gNuFj",
+});
+tracker.start();
+tracker.use(
+  trackerAssist({
+    onStart: ({ sessionID }) =>
+      console.log("OpenReplay tracker started with session: ", sessionID),
+  })
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<RouterProvider router={router} />
-		</Provider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
